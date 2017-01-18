76d49c064c0ec7842b6da0281c373732cf5a5cfe
root <tomolimo@gmail.com>
2017-01-18 11:08:48 +0100

    Added ru_RU, thanks to @AircraftRu Fixes #9

---
+++ locales/ru_RU.php
@@ -0,0 +1,14 @@
+<?php
+
+$LANG['timezones']['title']="Управления часовыми поясами";
+$LANG['timezones']['glpiversion']="Для этого плагина требуется GLPI >= 0.85";
+$LANG['timezones']['timezonetables']="mySQL 'time_zone' таблицы должны быть заполненны до установки этого плагина!<br>смотреть: <a href='http://dev.mysql.com/doc/refman/5.6/en/time-zone-support.html#time-zone-installation' target='_blank'>Заполнение Time Zone таблиц</a>";
+$LANG['timezones']['dbnotconverted']="'dbupdate' скрипт должен быть выполненным до включения этого плагина!<br>смотреть: <a href='https://forge.indepnet.net/projects/timezones/wiki/installation' target='_blank'>Смотрите Вики</a>";
+
+
+$LANG['timezones']['item']['tab']="Часовой пояс";
+$LANG['timezones']['item']['header']="Управление чамовым поясом";
+$LANG['timezones']['item']['dropdown']="Выбрать вашу Часовой пояс";
+$LANG['timezones']['item']['submit']="Сохранить";
+
+?>
