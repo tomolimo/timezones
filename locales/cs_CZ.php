7b08d556985026fca32f0bbdf3566f7648a15e49
p-bo <pavel.borecki@gmail.com>
2018-03-28 11:27:46 +0200

    Added Czech localization

---
+++ locales/cs_CZ.php
@@ -0,0 +1,13 @@
+<?php
+
+$LANG['timezones']['title']="Správa časových zón";
+$LANG['timezones']['glpiversion']="Tento zásuvný modul vyžaduje GLPI verze 0.85 a novější";
+$LANG['timezones']['timezonetables']="Před instalací tohoto zásuvného modulu je třeba naplnit MySQL tabulky „time_zone“!<br>viz: <a href='http://dev.mysql.com/doc/refman/5.6/en/time-zone-support.html#time-zone-installation'>Naplňování tabulek časových zón (v angličtině)</a>";
+$LANG['timezones']['dbnotconverted']="Před zapnutím tohoto zásuvného modulu je třeba spustit skript „dbupdate“!<br>viz: <a href='https://forge.indepnet.net/projects/timezones/wiki/installation' target='_blank'>wiki</a>";
+
+
+$LANG['timezones']['item']['tab']="Časová zóna";
+$LANG['timezones']['item']['header']="Správa časových zón";
+$LANG['timezones']['item']['dropdown']="Vyberte požadovanou časovou zónu";
+$LANG['timezones']['item']['submit']="Uložit";
+
